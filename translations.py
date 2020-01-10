004e9f46b9a9ff3067552c907346712d6c558818
Kevin Carrogan <kevin.carrogan@gmail.com>
2020-01-10 09:44:24 +0000

    Update translations

--- translations.py
+++ translations.py
@@ -1,3 +1,9 @@
 translations = [
+    ('Man', 'Girl'),
     ('Boss', 'Girlboss'),
+    ('Legend', 'Slut'),
+    ('Angry', 'On her period'),
+    ('Taking charge', 'Bossy'),
+    ('Has a job and family', 'Wants it all'),
+    ('Passionate', 'Aggressive'),
 ]
